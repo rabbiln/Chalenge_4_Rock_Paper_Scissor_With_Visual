bbe6a1b890841468e983e3cd15f45e23a8b773a5
rabbil <rabbil.nurrahmat@gmail.com>
2022-09-21 13:41:38 +0700

    change material implementation to lower version

--- app/src/main/java/com/guling/chalenge4rock_paper_scissor_withvisual/MainActivity.kt
+++ app/src/main/java/com/guling/chalenge4rock_paper_scissor_withvisual/MainActivity.kt
@@ -1,25 +1,42 @@
 package com.guling.chalenge4rock_paper_scissor_withvisual
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun Rock()= 0
-    fun Paper()= (-1)
-    fun Scissors()= (1)
 
-    fun compare_hands(hand1, hand2):
-            hand1=inp
-    winning_hand = 2  # this represents a draw
-    if hand1 != hand2:
-    if abs(hand1) == abs(hand2):
-    winning_hand = max(hand1, hand2)
-    else:
-    winning_hand = min(hand1, hand2)
-    return winning_hand
+   /* fun run() {
+        compare_hands()
+        result()
+        val rock = 0
+        val paper = (-1)
+        val scissors = (1)
     }
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            MainActivity().run()
+        }
+    }
+
+    private fun compare_hands(hand1, hand2) {
+        val hand1 = IOUtils.readString()
+        var winning_hand = 2
+        if (hand1 != hand2){
+            if((hand1) == (hand2)) {
+                winning_hand = max(hand1, hand2)
+            }
+        }
+         else {
+            winning_hand = min(hand1, hand2)
+            return winning_hand
+        }
+    }*/
+}
 
